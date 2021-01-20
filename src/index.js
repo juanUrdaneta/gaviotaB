0a6cc9fedb6a13ea809a86fb4e48d482b419c5f2
juan urdaneta <juankorus@gmail.com>
2021-01-20 02:34:34 -0300

    Added modal

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ tl.to(headerBefore, {
 tl.to(
   headerBefore,
   { duration: ANIM_DURATION * 1.5, height: "0rem", ease: "power2.out" },
-  "-=.4"
+  "-=4"
 );
 tl.to(".header", {
   duration: ANIM_DURATION * 1,
@@ -113,3 +113,76 @@ tl.from(
   },
   "-=1"
 );
+
+document
+  .querySelector(".modal__nav-left")
+  .addEventListener("mouseenter", () => {
+    gsap.to(".modal__content", {
+      duration: 1,
+      padding: "5rem 16rem",
+      ease: "elastic",
+    });
+    gsap.to(".modal__pictures__container", {
+      duration: 0.2,
+      borderRadius: "6rem",
+      ease: "power2.in",
+    });
+  });
+
+document
+  .querySelector(".modal__nav-right")
+  .addEventListener("mouseenter", () => {
+    gsap.to(".modal__content", {
+      duration: 1,
+      padding: "5rem 16rem",
+      ease: "elastic",
+    });
+    gsap.to(".modal__pictures__container", {
+      duration: 0.2,
+      borderRadius: "6rem",
+      ease: "power2.in",
+    });
+  });
+
+document
+  .querySelector(".modal__nav-left")
+  .addEventListener("mouseleave", () => {
+    gsap.to(".modal__content", {
+      duration: 0.4,
+      padding: "0",
+      ease: "power2.out",
+    });
+    gsap.to(".modal__pictures__container", {
+      duration: 0.2,
+      borderRadius: "0 0 0 6rem",
+      ease: "power2.in",
+    });
+  });
+
+document
+  .querySelector(".modal__nav-right")
+  .addEventListener("mouseleave", () => {
+    gsap.to(".modal__content", {
+      duration: 0.4,
+      padding: "0",
+      ease: "power2.out",
+    });
+    gsap.to(".modal__pictures__container", {
+      duration: 0.2,
+      borderRadius: "0 0 0 6rem",
+      ease: "power2.in",
+    });
+  });
+
+document.querySelector(".modal__content").addEventListener("mouseover", () => {
+  gsap.to(".modal__content", {
+    duration: 0.4,
+    padding: "0",
+    ease: "power2.out",
+  });
+  gsap.to(".modal__pictures__container", {
+    duration: 1,
+    borderRadius: "0 0 0 6rem",
+    ease: "power2.in",
+  });
+});
